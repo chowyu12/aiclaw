6abfd8d85a64cb928b0724e3756ab4fdb03658f1
joyz <chowyu12@163.com>
2026-03-27 10:08:28 +0800

    Add command-line argument handling for version display and self-update; implement version command to print current version and update command to trigger self-update process.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/chowyu12/aiclaw/internal/bootstrap"
+	"github.com/chowyu12/aiclaw/internal/selfupdate"
 )
 
-var configFile = flag.String("config", "", "config file path (default: ~/.aiclaw/config.yaml)")
+var version = "dev"
 
 func main() {
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "version":
+			fmt.Printf("aiclaw %s\n", version)
+			return
+		case "update":
+			selfupdate.Run(version)
+			return
+		}
+	}
+
+	configFile := flag.String("config", "", "config file path (default: ~/.aiclaw/config.yaml)")
 	flag.Parse()
 	bootstrap.Run(bootstrap.Options{ConfigFlag: *configFile})
 }
